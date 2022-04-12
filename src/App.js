c73016a745815a0bcca9a5c1430da45381f66d29
Jesse Schroeder <94391760+Jesse-Schroeder@users.noreply.github.com>
2022-04-12 12:49:13 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Quotes-Galore">
+      <header className="Click-Here-For-Wisdom">
+        <img src={logo} className="Quotes-Galore-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="Quotes-Galore-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
